eb69f2ba6ac0c62a572747690122522aaa75124f
youssefbenhissi <43909410+youssefbenhissi@users.noreply.github.com>
2018-10-21 16:16:22 +0100

    Add files via upload

---
+++ GoMyCode/js/main.js
@@ -0,0 +1,10 @@
+var a = document.querySelector('p').style.fontSize = "45px";
+var b = document.querySelector('p').style.backgroundColor = "red";
+var c = document.querySelector('p').style.color = "green";
+var d = document.querySelector("img").setAttribute("src", "you.jpg");
+var e = document.querySelector("p").innerHTML = "hahahhaa";
+
+function yo() {
+    document.querySelector('img').setAttribute("src", "yo");
+}
+setInterval(yo, "1000");
